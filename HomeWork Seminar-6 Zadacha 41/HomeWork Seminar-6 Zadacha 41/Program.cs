19eb4a4eb8c5993c9beecf75ab63f9801703ef9f
Alexandr <hitrin79@gmail.com>
2022-12-14 20:44:56 +0800

    Zadacha 41

---
+++ HomeWork Seminar-6 Zadacha 41/HomeWork Seminar-6 Zadacha 41/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите колличество чисел: ");
+int countNumbers = int.Parse(Console.ReadLine()!);
+int[] array = new int[countNumbers];
+
+void CreateMassive(int[] mass)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите число под индексом {i}: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    Console.WriteLine();
+    Console.Write("Вы ввели: ");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} | ");
+    }
+}
+
+int GetPositiveNumbers(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count ++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Колличество положительных чисел {count}");
+    return count;
+
+}
+
+CreateMassive(array);
+Console.WriteLine();
+GetPositiveNumbers(array);
